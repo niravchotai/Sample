c8ba9c0ac31d11804be1a4282d4802b805ec5d93
Joice <joicefootball@gmail.com>
2013-07-02 13:02:45 +0530

    Update primeNumber.js

--- primeNumber.js
+++ primeNumber.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 /**
- * Copyright  © 2013  Verifi, Corp. 
+ * Copyright  ï¿½ 2013  Verifi, Corp. 
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may obtain
