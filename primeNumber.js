ece382f27b3b2b7dc577cb47fbf313cc24fef8ed
Joice Joseph <joicefootball@gmail.com>
2013-07-02 08:19:29 +0100

    Periodical minor change

--- primeNumber.js
+++ primeNumber.js
@@ -13,17 +13,20 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
-
 //This commented area below  is a try. A try to recieve input from command prompt. but unfortunately it is not working. 
- 
- /*
+var getIn = function ()	{ 
 var numberFromPrompt=0;
-var prompts = rl.createInterface(process.stdin, process.stdout);
 var rl = require("readline");
-prompts.question("Untill How much do you wanna fing the prime Number Series? ", function  (numberFromPrompt) {
-	console.log(numberFromPrompt);
-		});*/
+var prompts = rl.createInterface(process.stdin, process.stdout);
+
+	prompts.question("Untill How much do you wanna fing the prime Number Series? ", function  (numberFromPrompt) {
+		console.log(numberFromPrompt);
+			});
+};
+/*
+the function above is not working as i decided. this function was designed to recieve maximum limit from the command line itself.
+need some modifications.
+*/
 var fs=require("fs"); //file stream object initialized
 var arr=[]; //array initialized, to push prime numbers 
 var outfile = "primeNumber.txt"; // file to be created and prime numbers to be shown
@@ -32,25 +35,25 @@ var outfile = "primeNumber.txt"; // file to be created and prime numbers to be s
 */
 var pusher = function(number) {
 	arr.push(number);
-		};		
+	};		
 /*
 the function fmt is used to concatanate items in the array with " " for console/file friendly output.
 */		
 var fmt = function(arr) {
 	return arr.join(" ");
-};		
+	};		
 /*
 This is the major function in this snippet. this function gives us the result that is the number is prime or not. it recieves input from 'loopMe' and returns is it a prime or not.
 */
 var isPrime = function(num) {
-if(num < 2)	{
-	return false;
-}
-for (var i = 2; i < num; i++)	{
-	if(num%i==0)	{
+	if(num < 2)	{
 		return false;
 	}
-}
+	for (var i = 2; i < num; i++)	{
+		if(num%i==0)	{
+			return false;
+		}
+	}
 return true;
 };
 var printMe =function()	{		
